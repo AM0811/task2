c3d75a37f9ba4353b729b868d8e52d7291044577
AM0811 <21445834@student.uwl.ac.uk>
2021-11-16 16:17:44 +0000

    3 files modified

--- src/MedicalTreatment.js
+++ src/MedicalTreatment.js
@@ -1,12 +1,8 @@
 import React from 'react';
-
-
-// 1 Component MedicalTreatment.js
-//  creating a function called MedicalTreatment with its parameters.
-export default function MedicalTreatment(props){
+function MedicalTreatment(props){
   return(
-    <div>
-      {props.name} {props.treatId} {props.treatCourseId} {props.type} {props.category}  {props.startDate}      
-      </div>
-  )
+    <li key= {props.treatId} > {props.treatCourseId} {props.type} {props.category} {props.name} {props.startDate} </li>
+  );
+
 }
+export default MedicalTreatment;
--- src/MedicalTreatmentList.js
+++ src/MedicalTreatmentList.js
@@ -1,8 +1,23 @@
 import React from 'react';
-function MedicalTreatmentList(){
-  return(
-    <li key= {props.treatId} > {props.treatCourseId} {props.type} {props.category} {props.name} {props.startDate} </li>
+import MedicalTreatment from './MedicalTreatment';
+
+function MedicalTreatmentList (){
+  const medicaltreatmentlistJson = '[{"treatId":1, "treatCourseId":"AS01", "type":"Radiation", "category":"Sarcoma", "name":"Brachyerapy", "startDate":"16/01/2021"},'+
+  '{"treatId":2, "treatCourseId":"AS02", "type":"Surgery", "category":"Carcinoma", "name":"Cryosurgery", "startDate":"18/03/2021"},'+
+'{"treatId":3, "treatCourseId":"AS03", "type":"Surgery", "category":"Carcinoma", "name":"Lasers", "startDate":"20/03/2021"},'+
+
+'{"treatId":4, "treatCourseId":"AS04", "type":"Surgery", "category":"Carcinoma", "name":"Photodynamic therapy", "startDate":"03/04/2021"},'+
+'{"treatId":5, "treatCourseId":"AS05", "type":"Chemotherapy", "category":"Carcinoma", "name":"Intravenous", "startDate":"05/05/2021"}]';
+
+  const medicaltreatmentlist = (JSON.parse(medicaltreatmentlistJson));
+
+  const listMedicalTreatmentList = medicaltreatmentlist.map((medicaltreatment) => <MedicalTreatment treatId={medicaltreatment.treatId} treatCourseId ={medicaltreatment.treatCourseId} type={medicaltreatment.type} category={medicaltreatment.category} name={medicaltreatment.name} startDate={medicaltreatment.startDate}/>
   );
 
+  return (
+    <div>
+      <ul>{listMedicalTreatmentList}</ul>
+    </div>
+  );
 }
 export default MedicalTreatmentList;
