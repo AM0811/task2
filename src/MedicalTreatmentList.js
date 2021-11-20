fefbc9aa20bfc26c95c33ab550c6fc31b5d4e555
AM0811 <21445834@student.uwl.ac.uk>
2021-11-20 00:46:08 +0000

    1 file modified

--- src/MedicalTreatmentList.js
+++ src/MedicalTreatmentList.js
@@ -3,10 +3,13 @@ import MedicalTreatment from './MedicalTreatment';
 
 function MedicalTreatmentList (){
   const medicaltreatmentlistJson = '[{"treatId":1, "treatCourseId":"AS01", "type":"Radiation", "category":"Sarcoma", "name":"Brachyerapy", "startDate":"16/01/2021"},'+
+
   '{"treatId":2, "treatCourseId":"AS02", "type":"Surgery", "category":"Carcinoma", "name":"Cryosurgery", "startDate":"18/03/2021"},'+
+
 '{"treatId":3, "treatCourseId":"AS03", "type":"Surgery", "category":"Carcinoma", "name":"Lasers", "startDate":"20/03/2021"},'+
 
 '{"treatId":4, "treatCourseId":"AS04", "type":"Surgery", "category":"Carcinoma", "name":"Photodynamic therapy", "startDate":"03/04/2021"},'+
+
 '{"treatId":5, "treatCourseId":"AS05", "type":"Chemotherapy", "category":"Carcinoma", "name":"Intravenous", "startDate":"05/05/2021"}]';
 
   const medicaltreatmentlist = (JSON.parse(medicaltreatmentlistJson));
